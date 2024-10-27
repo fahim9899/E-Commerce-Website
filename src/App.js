c358adf77a97d151bd91c180080255b8e784f3e9
fahim9899 <kfahim0619@gmail.com>
2024-10-27 13:47:39 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from '../src/assets/backgroud.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <h1>hi i am there</h1>
+       <h2>hi i am ther</h2>
+    </div>
+ 
+  );
+}
+
+export default App;
